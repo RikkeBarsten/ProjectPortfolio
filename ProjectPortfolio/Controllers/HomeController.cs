45efa072b60354322243d90fff2d5af1101bf8a6
Rikke Barsten <rikkebarsten@gmail.com>
2016-07-29 18:41:22 +0200

    Create new indicator controller + view

--- ProjectPortfolio/Controllers/HomeController.cs
+++ ProjectPortfolio/Controllers/HomeController.cs
@@ -12,41 +12,13 @@ namespace ProjectPortfolio.Controllers
 {
     public class HomeController : Controller
     {
-        private PortfolioContext db = new PortfolioContext();
+       
 
         public ActionResult Index()
         {
-            DashboardClassesCollection col = new DashboardClassesCollection();
+                    
 
-            // Setting up the list of ProjectBudget-items
-            var groupB = db.AProjects.GroupBy(p => p.StartDate.Year);
-            List<ProjectsBudget> dataB = new List<ProjectsBudget>();
-
-            foreach (var group in groupB)
-            {
-                decimal groupBudget = group.Sum(b => b.Budget);
-
-                dataB.Add(new ProjectsBudget {Year = group.Key, NoProjects = group.Count(), Budget = groupBudget});
-            }
-
-            //Add the list to the collection-class
-            col.BudgetList = dataB.OrderByDescending(p => p.Year).ToList();
-
-
-            // Set up the list of ProgramProject-class
-            var groupP = db.AProjects.GroupBy(p => p.ProgramId);
-            List<ProgramProjects> dataP = new List<ProgramProjects>();
-
-            foreach (var group in groupP)
-            {
-                dataP.Add(new ProgramProjects { Program = group.Key, NoProjects = group.Count() });
-            }
-
-            //Add the list to the collection class
-            col.ProgramList = dataP;
-           
-
-            return View(col);
+            return View();
         }
 
         public ActionResult Contact()
@@ -56,13 +28,6 @@ namespace ProjectPortfolio.Controllers
             return View();
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        
     }
 }
