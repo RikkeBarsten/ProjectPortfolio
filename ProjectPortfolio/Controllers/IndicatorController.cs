3764537c3c2328c6c3feb667a8d81ab4592b9dd2
RikkeBarsten <rikkebarsten@gmail.com>
2016-12-01 11:01:51 +0100

    Add projects to sunburst and fix extra last circle

--- ProjectPortfolio/Controllers/IndicatorController.cs
+++ ProjectPortfolio/Controllers/IndicatorController.cs
@@ -49,7 +49,7 @@ namespace ProjectPortfolio.Controllers
             List<Sequences> focusSequences = createSequencesList();
             List<Node> sequenceHierarchy = buildHierarchy(focusSequences);
 
-            ViewBag.SequencesCsv = sequenceStringCsv();
+            ViewBag.SequencesCsv = sequenceStringCsv2();
             ViewBag.Sequences = sequenceHierarchy.ToArray();
 
 
@@ -153,6 +153,60 @@ namespace ProjectPortfolio.Controllers
 
             return csvString.ToString();
             
+        }
+
+        private string sequenceStringCsv2()
+        {           
+            StringBuilder projectString = new StringBuilder();
+
+            // First create headlines
+            projectString.AppendLine("parent,child,project,budget,count");
+
+            //For each project - create sequence-string
+            foreach (var project in db.AProjects)
+            {
+
+                
+
+                // Only include projects from last 3 years
+                if (project.StartDate.Year > DateTime.Now.Year - 3)
+                {
+
+                    //Create the string-line ("primaryfocus-secondaryfocus-project-budget")
+                    //projectString.Append(project.ProgramId);
+
+                    //Check if primary focus is null
+                    if (project.PrimaryFocus != null)
+                    {
+                        projectString.Append(project.PrimaryFocus);
+                    }
+                    else
+                    {
+                        projectString.Append("ingen");
+                    }
+
+                    //Check if secondary focus is null
+                    if (project.SecondaryFocus != null)
+                    {
+                        projectString.Append("," + project.SecondaryFocus);
+                    }
+                    else
+                    {
+                        projectString.Append("," + "ingen");
+                    }
+
+                    projectString.Append("," + project.Name.Substring(0, (project.Name.Length < 25 ? project.Name.Length : 25)) + "...");
+
+                    int budget = project.Budget.HasValue ? (int)project.Budget : 0;
+
+                    projectString.Append("," + budget.ToString());
+                    projectString.Append("," + 1.ToString());
+
+                    //Create newline
+                    projectString.AppendLine();
+                }
+            }
+            return projectString.ToString();
         }
 
         private List<Sequences> createSequencesList()
